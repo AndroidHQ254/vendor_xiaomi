203d2590273d3ea7747bc355f426cf503e2c27e5
AndroidHQ254 <d.murimi14@gmail.com>
2023-07-15 17:15:32 +0300

    sm6150-common: Drop updatable GPU drivers

--- sm6150-common/sm6150-common-vendor.mk
+++ sm6150-common/sm6150-common-vendor.mk
@@ -719,7 +719,6 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.sm6150.api30 \
     PowerOffAlarm \
     DeviceInfo \
     ImsRcsService \
